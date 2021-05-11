fb0b3892f9f641376d6f1ed1fa9ddf365c0470ac
Danil Kalashnikov <scrolltex@gmail.com>
2021-05-11 12:50:32 +0700

    refactor(server): swagger auth

--- CybersecurityPortal.API/Startup.cs
+++ CybersecurityPortal.API/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -72,13 +73,37 @@ namespace CybersecurityPortal.API
             services.AddAutoMapper(typeof(Startup).Assembly);
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(opt =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CybersecurityPortal.API", Version = "v1" }); 
-                
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "CybersecurityPortal.API", Version = "v1" });
+
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
+                opt.IncludeXmlComments(xmlPath);
             });
 
             services
